af56f59eb170ed67495a503b42cd591253faa157
Limby72 <Limb720@gmail.com>
2018-08-09 11:40:32 -0400

    My initial project setup

---
+++ Objective-C Test/Objective-C Test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Objective-C Test
+//
+//  Created by Martin Nadeau on 2018-08-09.
+//  Copyright © 2018 AppPro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
